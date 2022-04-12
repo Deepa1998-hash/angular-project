5d665a6d1bdeba0a1a0b4b1eb150c0cf18227dab
deepa <deepaajay998@gmail.com>
2022-04-12 15:47:34 +0530

    lab submission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { ListitemComponent } from './listitem/listitem.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { AskquestionComponent } from './askquestion/askquestion.component';
+import { EditanswerComponent } from './editanswer/editanswer.component';
+import { MyanswersComponent } from './myanswers/myanswers.component';
+import { MyquestionsComponent } from './myquestions/myquestions.component';
+import { ShowquestionComponent } from './showquestion/showquestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginationComponent,
+    ListitemComponent,
+    HomeComponent,
+    AuthComponent,
+    AskquestionComponent,
+    EditanswerComponent,
+    MyanswersComponent,
+    MyquestionsComponent,
+    ShowquestionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/showquestion/showquestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showquestion',
+  templateUrl: './showquestion.component.html',
+  styleUrls: ['./showquestion.component.css']
+})
+export class ShowquestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
